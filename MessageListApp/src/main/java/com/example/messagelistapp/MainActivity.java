732a98b3be95d2bda9cc603535fd7f0791353240
IE93 <i.essessien@hotmail.com>
2014-04-06 01:03:34 +0100

    User input received via text field, displayed upon button press

--- MessageListApp/src/main/java/com/example/messagelistapp/MainActivity.java
+++ MessageListApp/src/main/java/com/example/messagelistapp/MainActivity.java
@@ -11,12 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
     }
 
 
@@ -54,7 +58,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        // Test the Button
-        mainTextView.setText("Button pressed!");
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
     }
 }
